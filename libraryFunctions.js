fe9e396e9773d96c48c53e0fc859dca121e6efc3
PriceLlewellyn <price13.llewellyn@gmail.com>
2025-07-04 14:08:31 +0100

    Initial commit

---
+++ libraryFunctions.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    
+    this.info = function() {
+        const readstatus =  this.readstatus ? "read already" : "waiting to read";
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readstatus}`;
+    };
+}
+
+const theHobbit = new info("The Hobbit", "J.R.R. Tolkien", 295 , false);
+console.log(theHobbit.info());
