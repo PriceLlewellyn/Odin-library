f8c0b8ffd14406ed0d3a0af9260af83f1beff842
PriceLlewellyn <price13.llewellyn@gmail.com>
2025-07-04 14:44:17 +0100

    Working function

--- libraryFunctions.js
+++ libraryFunctions.js
@@ -3,12 +3,12 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    
+
     this.info = function() {
-        const readstatus =  this.readstatus ? "read already" : "waiting to read";
+        const readstatus =  this.read ? "read already" : "waiting to read";
         return `${this.title} by ${this.author}, ${this.pages} pages, ${readstatus}`;
     };
 }
 
-const theHobbit = new info("The Hobbit", "J.R.R. Tolkien", 295 , false);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295 , false);
 console.log(theHobbit.info());
