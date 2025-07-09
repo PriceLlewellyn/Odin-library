6c8f6afa2199cbae0bf08505193e64c4d4cc57b7
PriceLlewellyn <price13.llewellyn@gmail.com>
2025-07-09 09:13:11 +0100

    formData to pull infor from form

--- libraryFunctions.js
+++ libraryFunctions.js
@@ -21,11 +21,27 @@ function Book(title, author, pages, read) {
     };
 }
 
+document.querySelector("#myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
 
+    let userInput = {};
+
+    for (const key of formData.keys()) {
+        const value = formData.get(key).toString();
+        if (value.length > 0) {
+            userInput[key] = value;
+        }
+    }
+    console.log(userInput);
+});
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295 , false);
 myLibrary.push(theHobbit)
 
 
-
+//pull info from form
+//pass infro into object
+//pass object into array
+//display array in table
 
